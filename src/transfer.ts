27feac81d9193b50c961972c461b508cab4e120e
Josh Crites <critesjosh@gmail.com>
2022-05-20 12:13:41 -0400

    dedup

--- src/transfer.ts
+++ src/transfer.ts
@@ -1,45 +1,20 @@
 // // https://github.com/AztecProtocol/aztec2-internal/blob/defi-bridge-project/end-to-end/src/sdk_utils.ts
 import { AccountId, AztecSdk, EthAddress, SchnorrSigner, TxSettlementTime } from "@aztec/sdk";
 
-export async function sendETH(
+export async function sendAsset(
   sender: AccountId,
   recipient: AccountId,
+  tokenAddress: EthAddress,
   tokenQuantity: bigint,
   settlementTime: TxSettlementTime,
   sdk: AztecSdk,
   signer: SchnorrSigner
 ) {
-  const tokenAssetId = sdk.getAssetIdBySymbol("ETH"); // can also use sdk.getAssetIdByAddress(token);
-  const tokenTransferFee = (await sdk.getTransferFees(tokenAssetId))[
+  const assetId = sdk.getAssetIdByAddress(tokenAddress);
+  const tokenTransferFee = (await sdk.getTransferFees(assetId))[
     settlementTime
   ];
-  const tokenAssetValue = { assetId: tokenAssetId, value: tokenQuantity };
-  const tokenTransferController = sdk.createTransferController(
-    sender,
-    signer,
-    tokenAssetValue,
-    tokenTransferFee,
-    recipient
-  );
-  await tokenTransferController.createProof();
-  await tokenTransferController.send();
-  return tokenTransferController;
-}
-
-export async function sendTokens(
-  sender: AccountId,
-  recipient: AccountId,
-  token: EthAddress,
-  tokenQuantity: bigint,
-  settlementTime: TxSettlementTime,
-  sdk: AztecSdk,
-  signer: SchnorrSigner
-) {
-  const tokenAssetId = sdk.getAssetIdByAddress(token);
-  const tokenTransferFee = (await sdk.getTransferFees(tokenAssetId))[
-    settlementTime
-  ];
-  const tokenAssetValue = { assetId: tokenAssetId, value: tokenQuantity };
+  const tokenAssetValue = { assetId, value: tokenQuantity };
   const tokenTransferController = sdk.createTransferController(
     sender,
     signer,
