fae6ffac3ecf8d383ba14f485d72babfd264b1d2
Josh Crites <critesjosh@gmail.com>
2022-05-20 14:50:04 -0400

    register account

---
+++ src/createAccount.ts
@@ -0,0 +1,42 @@
+import {
+  AccountId,
+  AztecSdk,
+  EthAddress,
+  SchnorrSigner,
+  TxSettlementTime
+} from "@aztec/sdk";
+
+export async function createAccount(
+  user: AccountId,
+  signer: SchnorrSigner,
+  alias: string,
+  newSigner: SchnorrSigner,
+  recoveryPublicKey,
+  tokenAddress: EthAddress,
+  tokenQuantity: bigint,
+  settlementTime: TxSettlementTime,
+  depositer: EthAddress,
+  sdk: AztecSdk
+) {
+  const assetId = sdk.getAssetIdByAddress(tokenAddress);
+  const tokenAssetValue = { assetId, value: tokenQuantity };
+  const txFee = (await sdk.getRegisterFees(tokenAssetValue))[settlementTime];
+
+  const controller = await sdk.createRegisterController(
+    user,
+    signer,
+    alias,
+    newSigner.getPublicKey(),
+    recoveryPublicKey,
+    tokenAssetValue,
+    txFee,
+    depositer
+  );
+
+  await controller.depositFundsToContract();
+  await controller.awaitDepositFundsToContract();
+
+  await controller.createProof();
+  await controller.sign();
+  await controller.send();
+}
