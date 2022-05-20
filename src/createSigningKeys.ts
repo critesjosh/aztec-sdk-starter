d299f9c7ebfd2642833b01a849f0f78411779750
Josh Crites <critesjosh@gmail.com>
2022-05-20 10:16:13 -0400

    add file

---
+++ src/createSigningKeys.ts
@@ -0,0 +1,34 @@
+import { AztecSdk, EthAddress, WalletProvider, Web3Signer } from "@aztec/sdk";
+
+const privateKeyMessage = Buffer.from(
+  `Sign this message to generate your Aztec Privacy Key. This key lets the application decrypt your balance on Aztec.\n\nIMPORTANT: Only sign this message if you trust the application.`
+);
+
+const spendingKeyMessage = Buffer.from(
+  `Sign this message to generate your Aztec Spending Key. This key lets the application spend your funds on Aztec.\n\nIMPORTANT: Only sign this message if you trust the application.`
+);
+
+export async function createSpendingKey(
+  provider: WalletProvider,
+  sdk: AztecSdk
+) {
+  const privateKey = await createSigningKey(provider, spendingKeyMessage);
+  const publicKey = await sdk.derivePublicKey(privateKey);
+  return { privateKey, publicKey };
+}
+
+export async function createPrivacyKey(
+  provider: WalletProvider,
+  sdk: AztecSdk
+) {
+  const privateKey = await createSigningKey(provider, privateKeyMessage);
+  const publicKey = await sdk.derivePublicKey(privateKey);
+  return { privateKey, publicKey };
+}
+
+const createSigningKey = async (provider: WalletProvider, message: Buffer) => {
+  const signer = new Web3Signer(provider);
+  const ethAddress = provider.getAccount(0);
+  const signedMessage = await signer.signMessage(message, ethAddress);
+  return signedMessage.slice(0, 32);
+};
