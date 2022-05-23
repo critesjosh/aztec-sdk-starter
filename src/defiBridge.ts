b220799a79d26efde769ad9601970ebec8f20f2e
Josh Crites <critesjosh@gmail.com>
2022-05-23 17:10:45 -0400

    add

--- src/defiBridge.ts
+++ src/defiBridge.ts
@@ -1,21 +1,26 @@
 import {
     AccountId,
+    AssetValue,
     AztecSdk,
     BridgeId,
+    DefiSettlementTime,
+    EthAddress,
     SchnorrSigner,
   } from "@aztec/sdk";
-
 
 export async function bridgeToDefi(
     user: AccountId,
     signer: SchnorrSigner,
     bridge: BridgeId,
-    depositValue,
-    fee,
+    tokenAddress: EthAddress,
+    tokenQuantity: bigint,
+    settlementTime: DefiSettlementTime,
     sdk: AztecSdk
 ){
-
-    const controller = sdk.createDefiController(user, signer, bridge, depositValue, fee);
+    const assetId = sdk.getAssetIdByAddress(tokenAddress);
+    const tokenAssetValue = { assetId, value: tokenQuantity };
+    const fee = (await sdk.getDefiFees(bridge))[settlementTime];
+    const controller = sdk.createDefiController(user, signer, bridge, tokenAssetValue, fee);
     await controller.createProof();
     await controller.send();
     return(await sdk.getDefiTxs(user));
