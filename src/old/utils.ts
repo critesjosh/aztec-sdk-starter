363df2d7372e3793961f0b88be05fba7d2789e51
Josh Crites <critesjosh@gmail.com>
2022-10-19 16:54:44 -0400

    clean src folder

--- src/old/utils.ts
+++
@@ -1,6 +0,0 @@
-export const formatAliasInput = (aliasInput: string) => aliasInput.toLowerCase();
-
-export const getAliasNonce = async (aliasInput: string, sdk) => {
-  const alias = formatAliasInput(aliasInput);
-  return sdk.getRemoteLatestAliasNonce(alias);
-};
